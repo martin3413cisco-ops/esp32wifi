147553cc252988b0cb5e44b9b7f6101230cf5f55
Martin <martin3413cisco@gmail.com>
2025-11-25 11:35:39 +0800

    web test

--- wifiscan/src/main.cpp
+++ wifiscan/src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
+#include <wifi.h>
+
+const char *ssid = "iPhone15proMax";
+const char *password= "m12345678m";
 
 // put function declarations here:
-int myFunction(int, int);
-
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status()!=WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+     }
+     Serial.println("WiFi Connected");
+     Serial.print("My IP= ");
+     Serial.print(WiFi.localIP());
+     Serial.print("Submask= ");
+     Serial.println(WiFi.subnetMask());
+ }
+  
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
